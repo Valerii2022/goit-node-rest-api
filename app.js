25b98cfaff6f7e85f1efe77b2b7b06ae57554721
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-05-26 18:59:33 +0300

    added connect to MongoDB

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(PORT, () => {
-      console.log("Server is running. Use our API on port: 3000");
+      console.log(`Server is running. Use our API on port: ${PORT}`);
     });
   })
   .catch((error) => {
