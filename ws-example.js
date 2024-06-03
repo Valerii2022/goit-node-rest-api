97c57079bc66b414114be3251ec7b1901f26a33c
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-06-03 12:06:55 +0300

    added websoket example

---
+++ ws-example.js
@@ -0,0 +1,17 @@
+import { WebSocketServer } from "ws";
+
+const wsServer = new WebSocketServer({ port: 5000 });
+
+const streamList = [];
+
+wsServer.on("connection", (stream) => {
+  console.log("New frontend connection");
+
+  setTimeout(() => stream.send("Welcome to web-socket server."), 3000);
+
+  streamList.forEach((item) =>
+    item.send(`We have ${streamList.length + 1} members.`)
+  );
+
+  streamList.push(stream);
+});
